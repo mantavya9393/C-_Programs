dd4bc0a756a2426eeb16312974b24fb4a9a35152
mantavya9393 <75443903+mantavya9393@users.noreply.github.com>
2021-07-13 18:13:01 +0530

    Add files via upload

---
+++ hybrid_in(stu).cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+class stu
+{
+	private:
+		int no;
+		char name[20];
+	public:
+		void setdata()
+		{
+			cout<<"\nEnter no:";
+			cin>>no;
+			
+			fflush(stdin);
+			cout<<"\nEnter name:";
+			gets(name);
+		}
+		void printdata()
+		{
+			cout<<"\nNo=>"<<no<<"\tName=>"<<name;
+		}
+};
+
+class marks
+{
+	protected:
+		int eng,hin;
+	public:
+		void setmark()
+		{
+			cout<<"\nEnter marks of English:";
+			cin>>eng;
+			cout<<"\nEnter marks of Hindi:";
+			cin>>hin;
+		}
+		void printmark()
+		{
+			cout<<"\tEnglish=>"<<eng;
+			cout<<"\tHindi=>"<<hin;
+		}
+};
+
+class sports: public stu
+{
+	protected:
+		int cri;
+	public:
+		void setcri()
+		{
+			cout<<"\nEnter marks of Cricket:";
+			cin>>cri;
+		}
+		void printcri()
+		{
+			cout<<"\tCricket=>"<<cri;
+		}
+};
+
+class result :public marks,public sports
+{
+	public:
+		void printres()
+		{
+			cout<<"\tTotal=>"<<eng+hin+cri;
+		}
+};
+
+main()
+{
+	result r1;
+	
+	r1.setdata();
+	r1.setmark();
+	r1.setcri();
+	
+	r1.printdata();
+	r1.printmark();
+	r1.printcri();
+	r1.printres();
+	
+}
